7f137fc68cd005c392f4ecb7eb7e66f9bf1cdc72
ROVNYAGIN Mikhail <Mikhail.ROVNYAGIN@raiffeisen.ru>
2026-03-12 00:52:38 +0300

    java zonky bumped

---
+++ src/main/java/digital/zil/hl/module1/configuration/ServicesConfig.java
@@ -0,0 +1,32 @@
+package digital.zil.hl.module1.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import digital.zil.hl.module1.entity.PhoneEntity;
+import digital.zil.hl.module1.entity.UserEntity;
+import digital.zil.hl.module1.repository.UserRepository;
+import digital.zil.hl.module1.service.UserService;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+@Configuration
+public class ServicesConfig {
+
+    @Bean
+    UserService userService(UserRepository userRepository) {
+        UserService userService = new UserService(userRepository);
+        for (int i = 0; i < 5; i++) {
+            UserEntity userEntity = new UserEntity();
+            userEntity.setIdentifier(UUID.randomUUID().toString());
+            userEntity.setFio("FIO" + i);
+            PhoneEntity phoneEntity = new PhoneEntity();
+            phoneEntity.setNumber("111111" + i);
+            phoneEntity.setUserEntity(userEntity);
+            userEntity.setPhones(Arrays.asList(phoneEntity));
+
+            userRepository.save(userEntity);
+        }
+        return userService;
+    }
+}
---
+++ src/main/java/digital/zil/hl/module1/controller/exeption/UserException.java
@@ -0,0 +1,8 @@
+package digital.zil.hl.module1.controller.exeption;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+}
