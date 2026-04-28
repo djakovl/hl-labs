3f853ea5441b98e44ffbb53326e73776fe1f318f
djakovl <djakovli@live.ru>
2026-04-28 13:02:37 +0300

    observ

--- 1.main/src/main/java/digital/zil/hl/module1/Application.java
+++ 1.main/src/main/java/digital/zil/hl/module1/Application.java
@@ -2,6 +2,7 @@ package digital.zil.hl.module1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling 
--- 2.additional/src/main/java/digital/zil/hl/additional/AdditionalApplication.java
+++ 2.additional/src/main/java/digital/zil/hl/additional/AdditionalApplication.java
@@ -2,6 +2,7 @@ package digital.zil.hl.additional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling 
