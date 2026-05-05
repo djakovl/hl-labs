ba4b2104e5890be801ecfd1cc0a1417fbb159afe
djakovl <djakovli@live.ru>
2026-05-05 10:55:22 +0300

    probes

---
+++ 1.main/src/main/java/digital/zil/hl/module1/health/KafkaHealthIndicator.java
@@ -0,0 +1,36 @@
+package digital.zil.hl.main.health;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+@Component("kafka")
+public class KafkaHealthIndicator implements HealthIndicator {
+
+    private final KafkaAdmin kafkaAdmin;
+
+    public KafkaHealthIndicator(KafkaAdmin kafkaAdmin) {
+        this.kafkaAdmin = kafkaAdmin;
+    }
+
+    @Override
+    public Health health() {
+        try (AdminClient client = AdminClient.create(kafkaAdmin.getConfigurationProperties())) {
+            var nodes = client.describeCluster()
+                    .nodes()
+                    .get(5, TimeUnit.SECONDS);
+            return Health.up()
+                    .withDetail("brokers", nodes.size())
+                    .build();
+        } catch (Exception e) {
+            return Health.down()
+                    .withDetail("error", e.getMessage())
+                    .build();
+        }
+    }
+}
