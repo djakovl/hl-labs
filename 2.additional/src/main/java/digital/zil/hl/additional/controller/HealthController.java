f5dc4c5855012d38c03bf35c4d468391025c396f
djakovl <djakovli@live.ru>
2026-05-05 12:59:36 +0300

    health fix

--- 2.additional/src/main/java/digital/zil/hl/additional/controller/HealthController.java
+++ 2.additional/src/main/java/digital/zil/hl/additional/controller/HealthController.java
@@ -1,13 +1,10 @@
 package digital.zil.hl.additional.controller;
 
-import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 public class HealthController {
